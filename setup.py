cc885106ea1b0c2e5089997e12494a8112e215d9
Robin k philip <robinkphilip@Robins-MacBook-Air.local>
2024-10-05 19:31:34 +0530

    setup-file-updation

--- setup.py
+++ setup.py
@@ -1,24 +1,41 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
+# Define a constant for the string '-e .' which is typically used in pip to install local packages.
 HYPEN_E_DOT = '-e .'
 
-def get_requirements(file_path:str)->List[str]:
-    """file return requirements list"""
-    requirements=[]
+def get_requirements(file_path: str) -> List[str]:
+    """
+    Reads the requirements file and returns a list of dependencies.
+    
+    Args:
+    file_path (str): Path to the requirements file.
+    
+    Returns:
+    List[str]: A list of package dependencies without newline characters.
+               If '-e .' is found, it is removed from the list as it is used 
+               to signify local package installation.
+    """
+    requirements = []
+    
+
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [ req.replace("\n","") for req in requirements ]
+
+        requirements = [req.replace("\n", "") for req in requirements]
+        
+        # Remove '-e .' if it is present, as it's used for local installation.
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-            
+    
     return requirements
 
+
 setup(
-    name='mlgenericproject',
-    version='0.0.1',
-    author='robin',
-    author_email='robinkphilip2001@gmail.com',
+    name='mlgenericproject',  
+    version='0.0.1',  
+    author='robin',  
+    author_email='robinkphilip2001@gmail.com',  
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt'),
 )
