49de1024ec90786dc8d718425500c5dfb8d45d12
Robin k philip <robinkphilip@Robins-MacBook-Air.local>
2024-10-13 12:16:01 +0530

    data tranfromation integrated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,6 +3,8 @@ import sys
 # custom made lib for logging and expection
 from src.exepection import  CustomExpection
 from src.logger import logging
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
 # lib tools
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -21,6 +23,7 @@ class DataIngestion:
         self.ingestion_config = DataIngestionConfig()
     
     def initiate_data_ingestion(self):
+        
         logging.info("Entered the data ingestion block")
         try:
             df = pd.read_csv('Dataset/stud.csv')
@@ -47,5 +50,9 @@ class DataIngestion:
             
 if __name__ == '__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initate_data_transformation(train_data,test_data)
+    
+    
         
