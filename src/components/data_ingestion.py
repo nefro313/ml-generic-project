590838de800c0058d1e0cca4b2d205573def3bc6
Robin k philip <robinkphilip@Robins-MacBook-Air.local>
2024-10-13 13:43:09 +0530

    model integrated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.exception import  CustomException
 from src.logger import logging
 from src.components.data_transformation import DataTransformationConfig
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 # lib tools
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -46,13 +48,16 @@ class DataIngestion:
                 self.ingestion_config.test_data_path
             )
         except Exception as e:
-            raise CustomExpection(e,sys)
+            raise CustomException(e,sys)
             
 if __name__ == '__main__':
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initate_data_transformation(train_data,test_data)
+    train_arr, test_arr,_ = data_transformation.initate_data_transformation(train_data,test_data)
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+
     
     
         
